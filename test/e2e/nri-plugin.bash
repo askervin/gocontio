0909a56b7e0d7f71923d4b5fb130b6b9903f6acf
Antti Kervinen <antti.kervinen@intel.com>
2020-11-06 15:01:09 +0200

    nri-plugin-wrapper: record and debug plugins on real work

--- test/e2e/nri-plugin.bash
+++ test/e2e/nri-plugin.bash
@@ -1,9 +1,10 @@
-out "install NRI plugin"
-NRI_BIN="$NRI_BIN_DIR/blockio-nri"
-[ -f "$NRI_BIN" ] ||
-    error "blockio-nri binary ($NRI_BIN) missing, run make"
-vm-put-file "$NRI_BIN" "/opt/nri/bin/$(basename "$NRI_BIN")"
-vm-put-file "$E2E_DIR/dlv-attach-NRIPLUGIN" "/opt/nri/bin/dlv-attach-NRIPLUGIN"
+# out "install NRI plugin"
+# NRI_BIN="$NRI_BIN_DIR/blockio-nri"
+# [ -f "$NRI_BIN" ] ||
+#     error "blockio-nri binary ($NRI_BIN) missing, run make"
+# vm-put-file "$NRI_BIN" "/opt/nri/bin/$(basename "$NRI_BIN")"
+vm-put-file "$E2E_DIR/nri-plugin-wrapper" "/opt/nri/bin/nri-plugin-wrapper"
+vm-command "ln -s /opt/nri/bin/nri-plugin-wrapper /opt/nri/bin/record-blockio-nri"
 
 out "configure NRI plugin"
 cat <<EOF | vm-pipe-to-file "/etc/nri/conf.json"
@@ -11,7 +12,7 @@ cat <<EOF | vm-pipe-to-file "/etc/nri/conf.json"
     "version": "0.1",
     "plugins": [
         {
-            "type": "blockio-nri",
+            "type": "record-blockio-nri",
             "conf": {
                 "myconf": "helloworld"
             }
